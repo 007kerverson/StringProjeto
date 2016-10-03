f14a8e777510bdb0946464b1ced4c321769ca8e1
Kerverson Gomes <kerversongomes@gmail.com>
2016-10-03 20:19:46 -0300

    Atividade sobre String

---
+++ Principal - Copia.py
@@ -0,0 +1,14 @@
+def output(Texto):
+    print(Texto)
+def tamanho(Texto):
+    tamanhotexto = len(Texto)
+    print("tamanhotexto", tamanho)
+    return tamanhotexto
+def maiusculo(Texto):
+    maiusculo = texto.upper()
+    print ("maiusculo", maiusculo)
+    return maiusculo
+def minusculo(Texto):
+    minusculo = texto.lower()
+    print ("minusculo", minusculo)
+    return minusculo
---
+++ StringUtil.py
@@ -0,0 +1,7 @@
+import StringUtil
+
+Texto = input("Digite o texto aqui:")
+StringUtil.output(Texto)
+StringUtil.tamanho(Texto)
+StringUtil.maiusculo(Texto)
+StringUtil.minusculo(Texto)
